53b576eecc64b12ba70952d7c2f16c96a9d7eab2
Yuki Ito <yuki@gnnk.net>
2013-04-21 07:40:47 +0900

    Clean up mix.exs

--- mix.exs
+++ mix.exs
@@ -3,18 +3,6 @@ defmodule DateTime.Mixfile do
 
   def project do
     [ app: :exdatetime,
-      version: "0.0.1",
-      deps: deps ]
-  end
-
-  # Configuration for the OTP application
-  def application do
-    []
-  end
-
-  # Returns the list of dependencies in the format:
-  # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
-  defp deps do
-    []
+      version: "0.0.1"]
   end
 end
